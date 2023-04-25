c9e183cb30ad66f625336c00180610473384c5da
QuentinAvrain <quentin.avrain@gmail.com>
2023-04-25 16:49:44 +0200

    scrollingY remade
    removed scrollbar, added scroll buttons, added anchor links

--- js/script.js
+++ js/script.js
@@ -2,52 +2,72 @@
 /* ----- ----- ----- HEADER ----- ----- ----- */
 /* ----- ----- ----- ------ ----- ----- ----- */
 const header = {
-        target : document.querySelector('header'),
-        button : document.querySelector('.headerToggleBtn'),
-        links : document.querySelectorAll('header a'),
-        logo : document.querySelector('header .logo'),
-        collapsed : false,
-        toggle : ()=> {
-            header.collapsed = !header.collapsed
-            if(header.collapsed === false){
-                header.target.classList.remove('collapsed');
-            } else {
-                header.target.classList.add('collapsed');
-            }
-            return header.collapsed;
-        }
+	target: document.querySelector("header"),
+	button: document.querySelector(".headerToggleBtn"),
+	toggle: () => {
+		if (header.target.classList.contains("collapsed")) {
+			header.target.classList.remove("collapsed");
+		} else {
+			header.target.classList.add("collapsed");
+		}
+		return header.collapsed;
+	},
 };
 
-header.button.addEventListener('click', header.toggle);
+header.button.addEventListener("click", header.toggle);
 
 /* ----- ----- ----- ------ ----- ----- ----- */
 /* ----- ----- ----- SCROLL ----- ----- ----- */
 /* ----- ----- ----- ------ ----- ----- ----- */
 const scroller = {
-    target : document.querySelector('.scrollContainer'),
-    tile : 0,
-    scroll : (event)=> {
-        const newTile = Math.round(scroller.target.scrollTop / (scroller.target.clientHeight));
+	target: document.querySelector(".scrollContainer"),
+	tile: 0,
+	scrollBtns: document.querySelectorAll(".scrollContainer > .btn"),
+	onScroll: (event) => {
+		const newTile = Math.round(
+			scroller.target.scrollTop / scroller.target.clientHeight
+		);
 
-        if(newTile != scroller.tile){
-
-            switch(newTile){
-                case 0 :
-                    header.button.classList.add('hidden');
-                    header.target.classList.remove('collapsed');
+		if (newTile != scroller.tile) {
+			switch (newTile) {
+				case 0:
+					header.button.classList.add("hidden");
+					scroller.scrollBtns[0].classList.add("hidden");
+					header.target.classList.remove("collapsed");
+					break;
+				case 1:
+					switch (scroller.tile) {
+						case 0:
+							header.button.classList.remove("hidden");
+							header.target.classList.add("collapsed");
+							scroller.scrollBtns[0].classList.remove("hidden");
+					}
+					break;
+				case 2:
+					switch (scroller.tile) {
+						case 3:
+							scroller.scrollBtns[1].classList.remove("hidden");
+					}
                     break;
-                case 1 :
-                    switch(scroller.tile){
-                        case 0 :
-                            header.button.classList.remove('hidden');
-                            header.target.classList.add('collapsed');
-                    }
+				case 3:
+					scroller.scrollBtns[1].classList.add("hidden");
                     break;
-            }
+			}
 
-            scroller.tile = newTile;
-        }
-    }
+			scroller.tile = newTile;
+		}
+	},
+	scroll: (direction) => {
+		console.log("scroll");
+		scroller.target.scroll({
+			top:
+				scroller.target.scrollTop +
+				direction * scroller.target.clientHeight,
+			behavior: "smooth",
+		});
+	},
 };
 
-scroller.target.addEventListener('scroll', (e)=>scroller.scroll(e))
+scroller.target.addEventListener("scroll", (e) => scroller.onScroll(e));
+scroller.scrollBtns[0].addEventListener("click", () => scroller.scroll(-1));
+scroller.scrollBtns[1].addEventListener("click", () => scroller.scroll(1));
