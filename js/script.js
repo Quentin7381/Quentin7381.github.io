c073479642e79a8090202b6e18643ecda2b53e59
Quentin Avrain <quentin.avrain@gmail.com>
2024-07-21 13:16:49 +0200

    Maj fonctionelle js

--- js/script.js
+++ js/script.js
@@ -41,11 +41,15 @@ const scroller = {
 	target: document.querySelector(".scrollContainer"),
 	tile: 0,
 	scrollBtns: document.querySelectorAll(".scrollContainer > .btn"),
+	maxTile: document.querySelectorAll(".scrollContainer > article").length - 1,
+	snapTimer: null,
+
 	/**
 	 * trigered by all scrollings. Dynamicly hide the menus
 	 * @param {*} event 
 	 */
 	onScroll: (event) => {
+		
 		//First we calc the tile we are arriving on
 		const newTile = Math.round(
 			scroller.target.scrollTop / scroller.target.clientHeight
@@ -53,45 +57,53 @@ const scroller = {
 		
 		//Then if we changed tile...
 		if (newTile != scroller.tile) {
-			//We check the tile we arrived on
-			switch (newTile) {
-				//If it is the first one
-				case 0:
-					scroller.scrollBtns[0].classList.add("hidden");
-					if(window.screen.width>AUTO_TOGGLE_WIDTH){
-						header.target.classList.remove("collapsed");
-						header.button.classList.add("hidden");
-					}
-					break;
-				//The second
-				case 1:
-					//Only if we came from the first one to the second one (so menu doesn't hide when scrolling upwards)
-					switch (scroller.tile) {
-						case 0:
-							scroller.scrollBtns[0].classList.remove("hidden");
-							header.target.classList.add("collapsed");
-							header.button.classList.remove("hidden");
-					}
-					break;
-				case 2:
-					switch (scroller.tile) {
-						case 3:
-							scroller.scrollBtns[1].classList.remove("hidden");
-					}
-					break;
-				case 3:
-					scroller.scrollBtns[1].classList.add("hidden");
-					break;
+
+			let direction = newTile - scroller.tile;
+
+			// For the first tile, the menu is displayed
+			if(newTile == 0) {
+				scroller.scrollBtns[0].classList.add("hidden");
+				scroller.scrollBtns[1].classList.remove("hidden");
+				if(window.screen.width>AUTO_TOGGLE_WIDTH){
+					header.target.classList.remove("collapsed");
+					header.button.classList.add("hidden");
+				}
+			}
+
+			// We hide the scroll bot button on last tile
+			else if(newTile == scroller.maxTile){
+				header.target.classList.add("collapsed");
+				scroller.scrollBtns[0].classList.remove("hidden");
+				scroller.scrollBtns[1].classList.add("hidden");
+			}
+
+			// We show scroll buttons on other tiles
+			else {
+				header.target.classList.add("collapsed");
+				scroller.scrollBtns[0].classList.remove("hidden");
+				scroller.scrollBtns[1].classList.remove("hidden");
 			}
 
 			scroller.tile = newTile;
 		}
+
+		if(typeof snapTimer != "undefined") {
+			clearTimeout(snapTimer);
+		}
+		snapTimer = setTimeout(() => {
+			console.log("Scrolling in 1 sec");
+			scroller.target.scroll({
+				top: scroller.tile * scroller.target.clientHeight,
+				behavior: "smooth",
+			});
+		}, 1000);
 
 		//Collapsing menu when screen size is small
 		if(window.screen.width<=AUTO_TOGGLE_WIDTH){
 			header.target.classList.add('collapsed');
 		}
 	},
+
 	/**
 	 * allow to scroll using events
 	 * @param {*} direction -1 is upward, 1 is downward. IMPORTANT NOTE : greater or lower numbers will multiply the scroll. To scroll 2 tiles up, you can use direction : -2;
@@ -104,6 +116,14 @@ const scroller = {
 			behavior: "smooth",
 		});
 	},
+
+	scrollTo: (tile) => {
+		scroller.target.scroll({
+			top: tile * scroller.target.clientHeight,
+			behavior: "smooth",
+		});
+	
+	}
 };
 
 scroller.target.addEventListener("scroll", (e) => scroller.onScroll(e));
@@ -113,7 +133,7 @@ scroller.scrollBtns[1].addEventListener("click", () => scroller.scroll(1));
 /* ----- ----- ----- -------------- ----- ----- ----- */
 /* ----- ----- ----- SMOOTH ANCHORS ----- ----- ----- */
 /* ----- ----- ----- -------------- ----- ----- ----- */
-//Prevent default anchor behaviour to insted scroll smoothly
+// Prevent default anchor behaviour to insted scroll smoothly
 
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 	anchor.addEventListener("click", (e) => {
